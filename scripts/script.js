7f1ba22d988e6fd49a9f9fe6c30b0f50f11ccf2b
GabrielHempel <gabriel.hempel@signoweb.com.br>
2023-10-27 13:09:06 -0300

    feat: update form validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
 const buttonForm = document.querySelector("#button-form");
 const form = document.querySelector("#form-promotion");
 
+const formContainer = document.querySelector(".form-container");
+const modalContainer = document.querySelector(".modal-container");
+
+const toast = document.querySelector(".toast");
+
 function formatPhoneNumber(input) {
   var cleaned = input.value.replace(/\D/g, "");
   var formatted = "";
@@ -25,20 +30,44 @@ form.addEventListener("submit", async (e) => {
 
   const formData = new FormData(form);
 
-  // try {
-  //   const response = await axios.post(
-  //     "http://localhost/api/register_promotion",
-  //     formData
-  //   );
+  try {
+    const response = await axios.post(
+      "https://europa.signo.dev.br/api/promo-filter",
+      formData
+    );
 
-  //   if (response.status === 201) {
-  //    console.log("deu boa")
+    if (response.status === 201) {
+      formContainer.classList.add("hide");
+      modalContainer.classList.add("active");
+      form.reset();
+    } else {
+      document.getElementById("apiMessage").textContent =
+        error.response.data.errors[0] != null
+          ? error.response.data.errors[0]
+          : "Não foi possível realizar o cadastro.";
 
-  //     form.reset();
-  //   } else {
-  //    console.log("não deu boa")
-  //   }
-  // } catch (error) {
-  //   console.log("ferrou geral")
-  // }
+      toast.classList.add("active");
+      toast.style.animation = "slideLeft 1.5s forwards";
+      setTimeout(() => {
+        toast.style.animation = "slideRight 1.5s forwards";
+      }, 12000);
+      setTimeout(() => {
+        toast.classList.remove("active");
+      }, 15000);
+      console.log("deu ruim");
+    }
+  } catch (error) {
+    console.log(error)
+
+    document.getElementById("apiMessage").textContent = "Não foi possível realizar o cadastro.";
+    
+    toast.classList.add("active");
+    toast.style.animation = "slideLeft 1.5s forwards";
+    setTimeout(() => {
+      toast.style.animation = "slideRight 1.5s forwards";
+    }, 12000);
+    setTimeout(() => {
+      toast.classList.remove("active");
+    }, 15000);
+  }
 });
