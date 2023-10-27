a865c7c089f9da75ee0fe0168115ff0b7ccfb102
GabrielHempel <gabriel.hempel@signoweb.com.br>
2023-10-27 10:50:20 -0300

    feat:final adjustment

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,44 @@
 const buttonForm = document.querySelector("#button-form");
-const form = document.querySelector("#form-promotion")
+const form = document.querySelector("#form-promotion");
+
+function formatPhoneNumber(input) {
+  var cleaned = input.value.replace(/\D/g, "");
+  var formatted = "";
+  for (var i = 0; i < cleaned.length; i++) {
+    if (i === 0) {
+      formatted += "(" + cleaned[i];
+    } else if (i === 1) {
+      formatted += cleaned[i] + ") ";
+    } else if (i === 2) {
+      formatted += cleaned[i] + " ";
+    } else if (i === 7) {
+      formatted += "-" + cleaned[i];
+    } else {
+      formatted += cleaned[i];
+    }
+  }
+  input.value = formatted;
+}
 
 form.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = new FormData(form);
-    console.log(formData);
-    // try {
-    //   const response = await axios.post(
-    //     "http://localhost/api/register_promotion",
-    //     formData
-    //   );
+  const formData = new FormData(form);
 
-    //   if (response.status === 201) {
-    //    console.log("deu boa")
+  // try {
+  //   const response = await axios.post(
+  //     "http://localhost/api/register_promotion",
+  //     formData
+  //   );
 
-    //     form.reset();
-    //   } else {
-    //    console.log("não deu boa")
-    //   }
-    // } catch (error) {
-    //   console.log("ferrou geral")
-    // }
+  //   if (response.status === 201) {
+  //    console.log("deu boa")
+
+  //     form.reset();
+  //   } else {
+  //    console.log("não deu boa")
+  //   }
+  // } catch (error) {
+  //   console.log("ferrou geral")
+  // }
 });
